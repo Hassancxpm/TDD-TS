a29180b51ee45b8ffb70bbb921079baa91fc27d3
Hassancxpm <hassan.akaou@comexposium.com>
2021-04-27 20:06:35 +0200

    change title calc

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -1,6 +1,6 @@
 import calc from './calc';
 
-describe('Calc', () => {
+describe('Calculations', () => {
     test('should return 10 for add(6, 4)', () => {
         expect(calc.add(6, 4)).toBe(10);
     });
