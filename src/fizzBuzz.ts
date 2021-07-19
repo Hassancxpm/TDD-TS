7d8df6d1be8a467d98b89777cb6b199c7c35891b
Hassancxpm <hassan.akaou@comexposium.com>
2021-07-19 18:30:05 +0200

    Add isZero method

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,11 +1,15 @@
 class fizzBuzz {
 
-  public evaluate (num: number) {
-    return num === 0 ? "0"
-      : this.isMultipleOf(num, 3*5) ? "FizzBuzz"
-      : this.isMultipleOf(num, 5) ? "Buzz"
-      : this.isMultipleOf(num, 3) ? "Fizz"
-      : num + ""
+  public evaluate(num: number) {
+    return this.isZero(num) ? "0"
+      : this.isMultipleOf(num, 3 * 5) ? "FizzBuzz"
+        : this.isMultipleOf(num, 5) ? "Buzz"
+          : this.isMultipleOf(num, 3) ? "Fizz"
+            : num + ""
+  }
+
+  private isZero(num: number): boolean {
+    return num === 0
   }
 
   private isMultipleOf(num: number, divisor: number): boolean {
